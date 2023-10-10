d9b281a26007b89ed7b44ccd274bdd4c5f4cb62b
blackhearrt <svmamonov36@gmail.com>
2023-10-10 21:51:04 +0300

    Adding a simple functional(return user id by reply)

---
+++ handlers.py
@@ -0,0 +1,18 @@
+from aiogram import types, F, Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+
+
+router = Router()
+
+
+
+@router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer("Hello! I'll help you to reveal your ID, just send me any message.")
+
+
+@router.message()
+async def message_handler(msg: Message):
+    await msg.answer(f"Your ID: {msg.from_user.id}")
